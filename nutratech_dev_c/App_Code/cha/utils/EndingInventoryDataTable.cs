27beb08e5ac96ffb205577ac2cdd6644ebb8807d
maine <mainevillarias@gmail.com>
2016-04-03 19:36:00 +0800

    updated EndingInventoryDataTable.cs

--- nutratech_dev_c/App_Code/cha/utils/EndingInventoryDataTable.cs
+++ nutratech_dev_c/App_Code/cha/utils/EndingInventoryDataTable.cs
@@ -1,11 +1,12 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+﻿
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 
 
 namespace cha.utils
@@ -300,9 +301,24 @@ namespace cha.utils
 
         private string DataTableToJSONWithJSONNet(DataTable table)
         {
+            /*
             string JsonString = string.Empty;
             JsonString = JsonConvert.SerializeObject(table, Formatting.None, new IsoDateTimeConverter() { DateTimeFormat = "MM/dd/yyyy" });
             return JsonString;
+            */
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Dictionary<string, object> childRow;
+            foreach (DataRow row in table.Rows)
+            {
+                childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    childRow.Add(col.ColumnName, row[col]);
+                }
+                parentRow.Add(childRow);
+            }
+            return jsSerializer.Serialize(parentRow);  
         }
 
   
